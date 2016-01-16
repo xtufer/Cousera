2b5b04dd9fccc1552e7fa6c75ff3b6972f041d0e
xtufer <cmoran@yahoo.com>
2016-01-16 11:46:38 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## function to store and invert a square matrix - assignment 2
 
 makeCacheMatrix <- function(x = matrix()) {
 
